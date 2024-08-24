f7c9c8488c655ec8e7c7d54da9d8b0a3f756f99c
AlejandroSantini <alejandro.j.santini@gmail.com>
2024-08-24 12:36:51 -0300

    config next, ts and tailwind proyect

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+      <main>
+        <h1 className="font-mono text-xl code">
+          Welcome <span className="text-blue-500">TOTS</span>
+        </h1>
+      </main>
+    </div>
+  );
+}

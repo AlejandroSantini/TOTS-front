b1ad5e21f4611067bdf13bdc69f68df18553aac8
AlejandroSantini <alejandro.j.santini@gmail.com>
2024-08-25 21:05:05 -0300

    add search bar

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet'; // Asegúrate de importar Leaflet
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { LatLngTuple } from 'leaflet';
 
@@ -17,14 +17,20 @@ const Map: React.FC<MapProps> = ({ countries }) => {
   });
 
   return (
-    <MapContainer center={[20, 0]} zoom={2} className="h-screen w-full rounded-lg mt-4">
+    <MapContainer center={[20, 0]} zoom={2} className="h-[80vh] w-full rounded-lg mt-4">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {countries.map((country, index) => (
         <Marker key={index} position={country.latlng} icon={customIcon}>
-          <Popup>{country.name}</Popup>
+          <Popup>
+            <div className="text-center">
+              <h2 className="text-lg font-bold">{country.name}</h2>
+              <p>Lat: {country.latlng[0]}</p>
+              <p>Lng: {country.latlng[1]}</p>
+            </div>
+          </Popup>
         </Marker>
       ))}
     </MapContainer>
--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -6,15 +6,16 @@ import Map from '../components/Map';
 import SearchBar from '../components/SearchBar';
 
 const Home = () => {
-  const [filter, setFilter] = useState('');
+  const [query, setQuery] = useState('');
   const [countries, setCountries] = useState<Country[]>([]);
+  const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
   const [error, setError] = useState<string | null>(null);
 
   const getCountries = async () => {
     try {
       const data = await fetchCountries();
       setCountries(data);
-      console.log(data);
+      setFilteredCountries(data);
     } catch (error) {
       setError((error as Error).message);
     }
@@ -25,17 +26,26 @@ const Home = () => {
   }, []);
 
   const handleSearch = (query: string) => {
-    setFilter(query);
+    setQuery(query);
+    filterCountries(query);
   };
 
-  const filteredCountries = countries.filter((country) =>
-    country.Country.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filterCountries = (query: string) => {
+    const lowercasedQuery = query.toLowerCase();
+    const filtered = countries.filter((country) => {
+      const nameMatches = country.Country.toLowerCase().includes(lowercasedQuery);
+      const codeMatches = country.ISOCode.toLowerCase().includes(lowercasedQuery); 
+      return nameMatches || codeMatches;
+    });
+    setFilteredCountries(filtered);
+  };
 
   return (
     <div className="container mx-auto p-4">
-      <div className="mb-4">
-        <h1 className="font-mono text-2xl font-bold text-center">TOTS <span className=''>Map</span></h1>
+      <div className="flex flex-col sm:flex-row justify-between items-center mt-4">
+        <h1 className="mb-3 sm:mb-0 font-mono text-2xl font-bold text-center">
+          <span className='text-blue-500'>TOTS</span> Map
+        </h1>
         <SearchBar onSearch={handleSearch} />
       </div>
       <Map
